1169da486b185353c0148e5897c2c310ea90811c
lukevictor <lucas.c.victor@hotmail.com>
2018-10-01 19:07:59 -0300

    Inicialização

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html class="full-height" lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BAMD Engenharia</title>
+    <meta name="description" content="Material design landing page template built by TemplateFlip.com"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <link href="styles/main.css" rel="stylesheet">
+  </head>
+  <body id="top">
+    <header>
+      <!-- Navbar-->
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar" id="navbar">
+        <div class="container"><a class="navbar-brand" href="#"><strong>BAMD Engenharia</strong></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a class="nav-link active" href="#about">Sobre</a></li>
+              <li class="nav-item"><a class="nav-link" href="#projects">Projetos</a></li>
+              <li class="nav-item"><a class="nav-link" href="#team">Responsavel</a></li>
+              <li class="nav-item"><a class="nav-link" href="#contact">Contato</a></li>
+  
+          </div>
+        </div>
+      </nav>
+      <!-- Intro Section-->
+      <section class="view hm-gradient" id="intro">
+        <div class="full-bg-img d-flex align-items-center">
+          <div class="container">
+            <div class="row no-gutters">
+              <div class="col-md-10 col-lg-6 text-center text-md-left margins">
+                <div class="white-text">
+                  <div class="wow fadeInLeft" data-wow-delay="0.3s">
+                    <h1 class="h1-responsive font-bold mt-sm-5">Your Success is Our Mission.</h1>
+                    <div class="h6">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt
+                      dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                      iste.
+                    </div>
+                  </div><br>
+                  <div class="wow fadeInLeft" data-wow-delay="0.3s"><a class="btn btn-white dark-grey-text font-bold ml-0" href="https://www.youtube.com/watch?v=60ItHLz5WEA" target="_blank"><i class="fa fa-play mr-1"></i> View Demo</a><a class="btn btn-outline-white" href="#">Learn more</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </header>
+    <div id="content">
+<section class="row no-gutters" id="features">
+  <div class="col-lg-3 col-md-6 col-sm-12 deep-purple lighten-1 text-white">
+    <div class="p-5 text-center wow zoomIn" data-wow-delay=".1s"><i class="fa fa-line-chart fa-2x"></i>
+      <div class="h5 mt-3">Agile Frameworks</div>
+      <p class="mt-5">Leverage agile frameworks to provide a robust synopsis for high level overviews.</p>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 purple lighten-1 text-white">
+    <div class="p-5 text-center wow zoomIn" data-wow-delay=".3s"><i class="fa fa-industry fa-2x"></i>
+      <div class="h5 mt-3">Corporate Strategy</div>
+      <p class="mt-5">Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition</p>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 teal lighten-1 text-white">
+    <div class="p-5 text-center wow zoomIn" data-wow-delay=".5s"><i class="fa fa-users fa-2x"></i>
+      <div class="h5 mt-3"> Workplace Diversity</div>
+      <p class="mt-5">Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 light-blue lighten-1 text-white">
+    <div class="p-5 text-center wow zoomIn" data-wow-delay=".7s"><i class="fa fa-bullhorn fa-2x"></i>
+      <div class="h5 mt-3"> Survival Strategies</div>
+      <p class="mt-5">Bring to the table win-win survival strategies to ensure proactive domination.</p>
+    </div>
+  </div>
+</section>
+<section class="text-center py-5 grey lighten-4" id="about">
+  <div class="container">
+    <div class="wow fadeIn">
+      <h2 class="h1 pt-5 pb-3">Why work with us?</h2>
+      <p class="px-5 mb-5 pb-3 lead blue-grey-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam.
+      </p>
+    </div>
+    <div class="row">
+      <div class="col-md-4 mb-r wow fadeInUp" data-wow-delay=".3s"><i class="fa fa-dashboard fa-3x orange-text"></i>
+        <h3 class="h4 mt-3">Design</h3>
+        <p class="mt-3 blue-grey-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti
+          hic.
+        </p>
+      </div>
+      <div class="col-md-4 mb-r wow fadeInUp" data-wow-delay=".4s"><i class="fa fa-comments-o fa-3x cyan-text"></i>
+        <h3 class="h4 mt-3">Feedback</h3>
+        <p class="mt-3 blue-grey-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti
+          hic.
+        </p>
+      </div>
+      <div class="col-md-4 mb-r wow fadeInUp" data-wow-delay=".5s"><i class="fa fa-cubes fa-3x red-text"></i>
+        <h3 class="h4 mt-3">Execution</h3>
+        <p class="mt-3 blue-grey-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti
+          hic.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="py-5" id="projects">
+  <div class="container">
+    <div class="wow fadeIn">
+      <h2 class="text-center h1 my-4">Our best projects</h2>
+      <p class="px-5 mb-5 pb-3 lead blue-grey-text text-center">
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div class="row wow fadeInLeft" data-wow-delay=".3s">
+      <div class="col-lg-6 col-xl-5 pr-lg-5 pb-5"><img class="img-fluid rounded z-depth-2" src="img/kuala-lumpur.jpg" alt="project image"/></div>
+      <div class="col-lg-6 col-xl-7 pl-lg-5 pb-4">
+        <div class="row mb-3">
+          <div class="col-1 mr-1"><i class="fa fa-book fa-2x cyan-text"></i></div>
+          <div class="col-10">
+            <h5 class="font-bold">Education</h5>
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda
+              voluptate velit.
+            </p>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-1 mr-1"><i class="fa fa-code fa-2x red-text"></i></div>
+          <div class="col-10">
+            <h5 class="font-bold">Technology</h5>
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda
+              voluptate velit.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-1 mr-1"><i class="fa fa-money fa-2x deep-purple-text"></i></div>
+          <div class="col-10">
+            <h5 class="font-bold">Finance</h5>
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda
+              voluptate velit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr/>
+    <div class="row pt-5 wow fadeInRight" data-wow-delay=".3s">
+      <div class="col-lg-6 col-xl-7 mb-3">
+        <div class="row mb-3">
+          <div class="col-1 mr-1"><i class="fa fa-bar-chart fa-2x indigo-text"></i></div>
+          <div class="col-10">
+            <h5 class="font-bold">Marketing</h5>
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda
+              voluptate velit.
+            </p>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-1 mr-1"><i class="fa fa-music fa-2x pink-text"></i></div>
+          <div class="col-10">
+            <h5 class="font-bold">Entertainment</h5>
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda
+              voluptate velit.
+            </p>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-1 mr-1"><i class="fa fa-smile-o fa-2x blue-text"></i></div>
+          <div class="col-10">
+            <h5 class="font-bold">Communication</h5>
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda
+              voluptate velit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 col-xl-5 mb-3"><img class="img-fluid rounded z-depth-2" src="img/san-francisco.jpg" alt="project image"/></div>
+    </div>
+  </div>
+</section>
+<section class="py-5" id="team">
+  <div class="container">
+    <div class="wow fadeIn">
+      <h2 class="h1 pt-5 pb-3 text-center">Sobre o responsavel</h2>
+      <p class="px-5 mb-5 pb-3 lead text-center blue-grey-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate
+        esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur accusamus veniam.
+      </p>
+    </div>
+    <div class="row mb-lg-4 center-on-small-only">
+  
+    <div class="row center-on-small-only">
+      <div class="col-lg-6 col-md-12 mb-r wow fadeInLeft" data-wow-delay=".3s">
+        <div class="col-md-6 float-left"><img class="img-fluid rounded z-depth-1 mb-3" src="img/man-1.jpg" alt="team member"/></div>
+        <div class="col-md-6 float-right">
+          <div class="h4">Bruno Antonio Machado Dias</div>
+          <h6 class="font-bold blue-grey-text mb-4">Engenheiro Civil</h6>
+          <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur.</p><a href="https://www.linkedin.com/in/bruno-dias-687365157/" target="_blank"><i class="fa fa-linkedin"></i><span class="ml-1">/in/bruno-dias-687365157/</span></a>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</section>
+<section id="contact" style="background-image:url('img/panorama-3094696_1920.jpg');">
+  <div class="rgba-black-strong py-5">
+    <div class="container">
+      <div class="wow fadeIn">
+        <h2 class="h1 text-white pt-5 pb-3 text-center">Entre em contato</h2>
+        <p class="text-white px-5 mb-5 pb-3 lead text-center">
+         ...
+        </p>
+      </div>
+      <div class="card mb-5 wow fadeInUp" data-wow-delay=".4s">
+        <div class="card-body p-5">
+          <div class="row">
+            <div class="col-md-8">
+              <form action="https://formspree.io/youremail@example.com" method="POST">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="md-form">
+                      <input class="form-control" id="name" type="text" name="name" required="required"/>
+                      <label for="name">Seu nome</label>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="md-form">
+                      <input class="form-control" id="email" type="text" name="_replyto" required="required"/>
+                      <label for="email">Seu email</label>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="md-form">
+                      <input class="form-control" id="subject" type="text" name="subject" required="required"/>
+                      <label for="subject">Assunto</label>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="md-form">
+                      <textarea class="md-textarea" id="message" name="message" required="required"></textarea>
+                      <label for="message">Sua mensagem</label>
+                    </div>
+                  </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+                </div>
+                <div class="center-on-small-only mb-4">
+                  <button class="btn btn-indigo ml-0" type="submit"><i class="fa fa-paper-plane-o mr-2"></i> Enviar</button>
+                </div>
+              </form>
+            </div>
+            <div class="col-md-4">
+              <ul class="list-unstyled text-center">
+                <li class="mt-4"><i class="fa fa-map-marker indigo-text fa-2x"></i>
+                  <p class="mt-2">Av. Ascaz, Sao Paulo, SP</p>
+                </li>
+                <li class="mt-4"><i class="fa fa-phone indigo-text fa-2x"></i>
+                  <p class="mt-2">+ (11) celular</p>
+                </li>
+                <li class="mt-4"><i class="fa fa-envelope indigo-text fa-2x"></i>
+                  <p class="mt-2">bamd@engenharia.com</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section></div>
+    <footer class="page-footer indigo darken-2 center-on-small-only pt-0 mt-0">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="mb-5 flex-center"><a class="px-3"><i class="fa fa-facebook fa-lg white-text"></i></a><a class="px-3"><i class="fa fa-twitter fa-lg white-text"></i></a><a class="px-3"><i class="fa fa-google-plus fa-lg white-text"></i></a><a class="px-3"><i class="fa fa-linkedin fa-lg white-text"></i></a></div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container-fluid">
+          <p>&copy; <a href="/">Material Landing</a> - Design: <a href="https://templateflip.com/" target="_blank">TemplateFlip</a></p>
+        </div>
+      </div>
+    </footer>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script>new WOW().init();</script>
+  </body>
+</html>
